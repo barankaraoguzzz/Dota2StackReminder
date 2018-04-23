1435f6e115dd6ab74741379997891a6e398d0680
Baran <b.b.karaoguz@gmail.com>
2018-04-24 00:14:18 +0300

    Initial Commit

---
+++ Dota/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dota
+//
+//  Created by Baran on 24.04.2018.
+//  Copyright © 2018 CodingMind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
